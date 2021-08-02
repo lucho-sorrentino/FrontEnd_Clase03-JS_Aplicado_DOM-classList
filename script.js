7215f2881f38bff17ba50caedfad62ac1b42e8a7
Lucho Sorrentino <lucianomartin.sorrentino@gmail.com>
2021-08-02 17:42:55 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,22 @@
+//////////////////// EJERCICIOS ClassList ////////////////////
+
+//     ********** Ejercicio 01 **********
+// Crear un párrafo. Luego desde el javascript hacer que se vea rojo.
+// document.querySelector("#uno").classList.add("red")
+
+// console.log(document.querySelector("#uno").classList);  //con classList "solo" puedo ver las clases que tiene el elemento.
+
+
+//     ********** Ejercicio 02 **********
+// Crear tres párrafos. Luego desde el javascript hacer que tengan un fondo verde.
+document.querySelectorAll("#dos").forEach(e => e.classList.add("fondo-verde"))
+
+
+
+//     ********** Ejercicio 01 **********
+// Crear 4 párrafos, dos con clase “blanco”. A todos agregarle un fondo celeste, y a los clase blanco además hacer que el texto se vea blanco.
+
+
+//     ********** Ejercicio 01 **********
+// En base al siguiente listado de notas, mostrar en verde los mayores a 7, amarillo los que están entre 4 y 6 y en rojo del tres para abajo.
+// notas = [9,2,7,6,6,4,5,10,3,9.5]
